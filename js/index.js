e062ee6c0e0549b5a69d9f0fad8214408a8c49d6
nori122 <norito.sasaki.jp@gmail.com>
2020-06-06 23:49:41 +0900

    be4 store cre8

--- js/index.js
+++ js/index.js
@@ -2,9 +2,11 @@ new Vue({
     el: '#app',
     // data: "",
     methods: {
+        //"create_store.html"に遷移
         moveToStoreCreate: function () {
             location.href = "store_create.html";
         },
+        //"store_list.html"に遷移
         moveToStoreList: function () {
             location.href = "https://www.google.com/"
         }
---
+++ js/store_create.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    // data: "",
+    methods: {
+        //"create_menu.html"に遷移
+        moveToMenuCreate: function () {
+            location.href = "menu_create.html";
+        },
+    }
+});
