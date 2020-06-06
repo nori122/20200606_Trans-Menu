c0652ac65d609c9126032bbafa18607eb58caa5a
nori122 <norito.sasaki.jp@gmail.com>
2020-06-06 23:03:40 +0900

    index created

---
+++ js/index.js
@@ -0,0 +1,12 @@
+new Vue({
+    el: '#app',
+    // data: "",
+    methods: {
+        moveToStoreCreate: function () {
+            location.href = "store_create.html";
+        },
+        moveToStoreList: function () {
+            location.href = "https://www.google.com/"
+        }
+    }
+});
