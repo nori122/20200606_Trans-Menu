33db397e914bbe7b9e07ea380d01bef7f27ec426
nori122 <norito.sasaki.jp@gmail.com>
2020-06-07 20:46:01 +0900

    before merging india to original

--- js/StoreCreate.js
+++ js/StoreCreate.js
@@ -29,27 +29,44 @@ new Vue({
         },
         downloadImgFromDB: function () {
         },
+        uploadImage() {
+            const ref = firebase.storage().ref()
+            const file = document.querySelector("#photo").files[0]
+            const name = new Date() + '-' + file.name
+            const metadata = {
+                contentType: file.type
+            }
+            const task = ref.child(name).put(file, metadata)
+            task
+                .then(snapshot => snapshot.ref.getDownloadURL())
+                .then(url => {
+                    console.log(url)
+                    alert("Image upload sucessfully")
+                    const image = document.querySelector('#image')
+                    image.src
+                })
+        }
     }
 });
 
-function uploadImage() {
-    const ref = firebase.storage().ref()
-    const file = document.querySelector("#photo").files[0]
-    const name = new Date() + '-' + file.name
-    const metadata = {
-        contentType: file.type
-    }
-    const task = ref.child(name).put(file, metadata)
-    task
-        .then(snapshot => snapshot.ref.getDownloadURL())
-        .then(url => {
-            console.log(url)
-            alert("Image upload sucessfully")
-            const image = document.querySelector('#image')
-            image.src
-        })
+// function uploadImage() {
+//     const ref = firebase.storage().ref()
+//     const file = document.querySelector("#photo").files[0]
+//     const name = new Date() + '-' + file.name
+//     const metadata = {
+//         contentType: file.type
+//     }
+//     const task = ref.child(name).put(file, metadata)
+//     task
+//         .then(snapshot => snapshot.ref.getDownloadURL())
+//         .then(url => {
+//             console.log(url)
+//             alert("Image upload sucessfully")
+//             const image = document.querySelector('#image')
+//             image.src
+//         })
 
-}
+// }
 
 //memo
 // done! ・店名をDBへアップロードの関数を作成
