5c96f5a7a8fcb3bb0ae161aef25c866b0b2303c3
nori122 <norito.sasaki.jp@gmail.com>
2020-06-07 20:35:13 +0900

    transfered script from html to js

--- js/StoreCreate.js
+++ js/StoreCreate.js
@@ -32,7 +32,24 @@ new Vue({
     }
 });
 
+function uploadImage() {
+    const ref = firebase.storage().ref()
+    const file = document.querySelector("#photo").files[0]
+    const name = new Date() + '-' + file.name
+    const metadata = {
+        contentType: file.type
+    }
+    const task = ref.child(name).put(file, metadata)
+    task
+        .then(snapshot => snapshot.ref.getDownloadURL())
+        .then(url => {
+            console.log(url)
+            alert("Image upload sucessfully")
+            const image = document.querySelector('#image')
+            image.src
+        })
 
+}
 
 //memo
 // done! ・店名をDBへアップロードの関数を作成
