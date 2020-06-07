dd8f9acb6ab7cb42cf2e2cc1a5106b9267a74ed1
nori122 <norito.sasaki.jp@gmail.com>
2020-06-07 11:25:33 +0900

    0607 stareted

--- js/StoreCreate.js
+++ js/StoreCreate.js
@@ -2,14 +2,18 @@ new Vue({
     el: '#app',
     data: function () {
         return {
-            storeName: '店舗名を入力してくださいね',
-            storePicPath: './img/store.jpg',
+            storeName: '',
+            storePicPath: '',
         };
     },
     methods: {
         //店舗画像をアップロードする
         uploadStorePic: function () {
-
+            const files = this.$refs.upfile;
+            const fileImg = files.files[0];
+            if (fileImg.type.startsWith("image/")) {
+                this.storePicPath = window.URL.createObjectURL(fileImg);
+            }
         },
         //'MenuCreate.html'に遷移
         moveToMenuCreate: function () {
