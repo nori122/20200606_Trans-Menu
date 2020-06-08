f7b038ef20e8812db49aa9ba9ca32c38d86e2acb
nori122 <norito.sasaki.jp@gmail.com>
2020-06-08 18:29:01 +0900

    MenuCreate_4items coded, pics resized

--- js/MenuCreate.js
+++ js/MenuCreate.js
@@ -21,8 +21,11 @@ new Vue({
         pushItemsToDB: function () {
             this.pushItem1toDB()
             this.pushItem2toDB()
+            this.pushItem3toDB()
+            this.pushItem4toDB()
         },
 
+        //item1の登録
         pushItem1toDB: function () {
             //Storageに商品画像を追加
             const ref = firebase.storage().ref()
@@ -35,8 +38,8 @@ new Vue({
             task
                 .then(snapshot => snapshot.ref.getDownloadURL())
                 .then(url => {
-                    console.log(url)
-                    alert("店舗登録が完了しました")
+                    console.log('item1のURLは' + url)
+                    // alert("登録が完了しました")
 
                     //#imageにURLを突っ込む方法
                     //できればv-bindでここに描画したいので、これは妥協案
@@ -48,22 +51,12 @@ new Vue({
                         itemName1: this.itemName1,
                         itemPrice1: this.itemPrice1,
                         itemPicPath1: url,
-
-                        // itemName2: this.itemName2,
-                        // itemPrice2: this.itemPrice2,
-                        // itemPicPath2: url,
-
-                        // itemName3: this.itemName3,
-                        // itemPrice3: this.itemPrice3,
-                        // itemPicPath3: url,
-
-                        // itemName4: this.itemName4,
-                        // itemPrice4: this.itemPrice4,
-                        // itemPicPath4: url,
                     });
                 })
 
         },
+
+        //item2の登録
         pushItem2toDB: function () {
             //Storageに商品画像を追加
             const ref = firebase.storage().ref()
@@ -76,9 +69,7 @@ new Vue({
             task
                 .then(snapshot => snapshot.ref.getDownloadURL())
                 .then(url => {
-                    console.log(url)
-                    alert("店舗登録が完了しました")
-
+                    console.log('item2のURLは' + url)
                     //#imageにURLを突っ込む方法
                     //できればv-bindでここに描画したいので、これは妥協案
                     const image = document.querySelector('#itemPreview2')
@@ -86,10 +77,6 @@ new Vue({
 
                     //Cloud Firestoreに店舗名と画像URLを追加
                     itemDB.add({
-                        // itemName1: this.itemName1,
-                        // itemPrice1: this.itemPrice1,
-                        // itemPicPath1: url,
-
                         itemName2: this.itemName2,
                         itemPrice2: this.itemPrice2,
                         itemPicPath2: url,
@@ -102,6 +89,70 @@ new Vue({
                         // itemPrice4: this.itemPrice4,
                         // itemPicPath4: url,
                     });
+                    alert("画像登録が完了しました")
+
+                })
+
+        },
+
+        //item3の登録
+        pushItem3toDB: function () {
+            //Storageに商品画像を追加
+            const ref = firebase.storage().ref()
+            const file = document.querySelector("#itemPic3").files[0]
+            const name = new Date() + '_' + file.name
+            const metadata = {
+                contentType: file.type
+            }
+            const task = ref.child('images/' + name).put(file, metadata)
+            task
+                .then(snapshot => snapshot.ref.getDownloadURL())
+                .then(url => {
+                    console.log('item3のURLは' + url)
+                    // alert("登録が完了しました")
+
+                    //#imageにURLを突っ込む方法
+                    //できればv-bindでここに描画したいので、これは妥協案
+                    const image = document.querySelector('#itemPreview3')
+                    image.src = url
+
+                    //Cloud Firestoreに店舗名と画像URLを追加
+                    itemDB.add({
+                        itemName3: this.itemName3,
+                        itemPrice3: this.itemPrice3,
+                        itemPicPath3: url,
+                    });
+                })
+
+        },
+
+        //item4の登録
+        pushItem4toDB: function () {
+            //Storageに商品画像を追加
+            const ref = firebase.storage().ref()
+            const file = document.querySelector("#itemPic4").files[0]
+            const name = new Date() + '_' + file.name
+            const metadata = {
+                contentType: file.type
+            }
+            const task = ref.child('images/' + name).put(file, metadata)
+            task
+                .then(snapshot => snapshot.ref.getDownloadURL())
+                .then(url => {
+                    console.log('item4のURLは' + url)
+                    //#imageにURLを突っ込む方法
+                    //できればv-bindでここに描画したいので、これは妥協案
+                    const image = document.querySelector('#itemPreview4')
+                    image.src = url
+
+                    //Cloud Firestoreに店舗名と画像URLを追加
+                    itemDB.add({
+                        itemName4: this.itemName4,
+                        itemPrice4: this.itemPrice4,
+                        itemPicPath4: url,
+                    });
+                    alert("画像登録が完了しました")
+
                 })
 
         },
