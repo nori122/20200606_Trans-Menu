2f9b1492544acbc830d6a0a1673ff539d75e9950
nori122 <norito.sasaki.jp@gmail.com>
2020-06-08 21:32:59 +0900

    CreateStore.html_before nesting Vue instance

---
+++ js/CreatedStore.js
@@ -0,0 +1,36 @@
+new Vue({
+    el: '#app',
+    data: function () {
+        return {
+            //店舗情報
+            storeName: 'aaa',
+            storePicPath: '',
+            storeColor: '',
+
+            //メニュー情報
+            itemName1: '',
+            itemPrice1: '',
+            itemPicPath1: '',
+            itemName2: '',
+            itemPrice2: '',
+            itemPicPath2: '',
+            itemName3: '',
+            itemPrice3: '',
+            itemPicPath3: '',
+            itemName4: '',
+            itemPrice4: '',
+            itemPicPath4: '',
+        };
+    },
+
+    methods: {
+
+    },
+
+
+});
+
+
+
+//memo
+// 
