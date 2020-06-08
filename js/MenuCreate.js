0b77d124f2dfe5f9a11b47414ad997b411f4cffb
nori122 <norito.sasaki.jp@gmail.com>
2020-06-08 18:13:31 +0900

    menuCreate_looks fine for duplicating

--- js/MenuCreate.js
+++ js/MenuCreate.js
@@ -18,8 +18,12 @@ new Vue({
     },
     methods: {
         //フォームの入力内容をCloud Firestoreとstorageに送信
-        pushItemToDB: function () {
+        pushItemsToDB: function () {
+            this.pushItem1toDB()
+            this.pushItem2toDB()
+        },
 
+        pushItem1toDB: function () {
             //Storageに商品画像を追加
             const ref = firebase.storage().ref()
             const file = document.querySelector("#itemPic1").files[0]
@@ -45,17 +49,58 @@ new Vue({
                         itemPrice1: this.itemPrice1,
                         itemPicPath1: url,
 
+                        // itemName2: this.itemName2,
+                        // itemPrice2: this.itemPrice2,
+                        // itemPicPath2: url,
+
+                        // itemName3: this.itemName3,
+                        // itemPrice3: this.itemPrice3,
+                        // itemPicPath3: url,
+
+                        // itemName4: this.itemName4,
+                        // itemPrice4: this.itemPrice4,
+                        // itemPicPath4: url,
+                    });
+                })
+
+        },
+        pushItem2toDB: function () {
+            //Storageに商品画像を追加
+            const ref = firebase.storage().ref()
+            const file = document.querySelector("#itemPic2").files[0]
+            const name = new Date() + '_' + file.name
+            const metadata = {
+                contentType: file.type
+            }
+            const task = ref.child('images/' + name).put(file, metadata)
+            task
+                .then(snapshot => snapshot.ref.getDownloadURL())
+                .then(url => {
+                    console.log(url)
+                    alert("店舗登録が完了しました")
+
+                    //#imageにURLを突っ込む方法
+                    //できればv-bindでここに描画したいので、これは妥協案
+                    const image = document.querySelector('#itemPreview2')
+                    image.src = url
+
+                    //Cloud Firestoreに店舗名と画像URLを追加
+                    itemDB.add({
+                        // itemName1: this.itemName1,
+                        // itemPrice1: this.itemPrice1,
+                        // itemPicPath1: url,
+
                         itemName2: this.itemName2,
                         itemPrice2: this.itemPrice2,
                         itemPicPath2: url,
 
-                        itemName3: this.itemName3,
-                        itemPrice3: this.itemPrice3,
-                        itemPicPath3: url,
+                        // itemName3: this.itemName3,
+                        // itemPrice3: this.itemPrice3,
+                        // itemPicPath3: url,
 
-                        itemName4: this.itemName4,
-                        itemPrice4: this.itemPrice4,
-                        itemPicPath4: url,
+                        // itemName4: this.itemName4,
+                        // itemPrice4: this.itemPrice4,
+                        // itemPicPath4: url,
                     });
                 })
 
