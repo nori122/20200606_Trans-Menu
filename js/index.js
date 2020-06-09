5b53a9960addf30705f53a7cda92b29c16a9ca31
nori122 <norito.sasaki.jp@gmail.com>
2020-06-10 00:10:44 +0900

    Index_designed

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-new Vue({
-    el: '#app',
-    // data: '',
-    methods: {
-        //'StoreCreate.html'に遷移
-        moveToStoreCreate: function () {
-            location.href = 'StoreCreate.html';
-        },
-        //'StoreList.html'に遷移
-        moveToStoreList: function () {
-            location.href = 'https://www.google.com/'
-        }
-    }
-});
+// new Vue({
+//     el: '#app',
+//     // data: '',
+//     methods: {
+//         //'StoreCreate.html'に遷移
+//         moveToStoreCreate: function () {
+//             location.href = 'StoreCreate.html';
+//         },
+//         //'StoreList.html'に遷移
+//         moveToStoreList: function () {
+//             location.href = 'https://www.google.com/'
+//         }
+//     }
+// });
